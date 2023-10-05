de24e5a82bf0ca3248e9be5c16d8f8480e3f8e87
shramitgupta <shramitgupta1212@gmail.com>
2023-10-06 03:15:53 +0530

    first commit

---
+++ lib/Auth/Admin_auth/admin_signup.dart
@@ -0,0 +1,263 @@
+import 'dart:developer';
+import 'package:battery_service_app/Auth/Admin_auth/adminlogin.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AdminSignUp extends StatefulWidget {
+  const AdminSignUp({Key? key}) : super(key: key);
+
+  @override
+  _AdminSignUpState createState() => _AdminSignUpState();
+}
+
+class _AdminSignUpState extends State<AdminSignUp> {
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  bool isSigningUp = false; // Track signup state
+  Future<void> _signUp() async {
+    try {
+      setState(() {
+        isSigningUp = true; // Show loading indicator
+      });
+
+      // Firebase signup logic
+      log('Signing up...');
+      final authResult =
+          await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: emailController.text,
+        password: passwordController.text,
+      );
+      final user = authResult.user;
+
+      if (user != null) {
+        log('User email: ${user.email}');
+        await FirebaseFirestore.instance.collection('Admin').doc(user.uid).set({
+          'email': user.email,
+        });
+        log('Email uploaded to Firestore.');
+      } else {
+        log('User is null.');
+      }
+
+      // If signup is successful, show a success notification
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text('Sign up successful!'),
+          duration: Duration(seconds: 2),
+        ),
+      );
+
+      // Upload the user's email to Firestore under the "Admin" collection
+      if (user != null) {
+        await FirebaseFirestore.instance.collection('Admin').doc(user.uid).set({
+          'email': user.email,
+        });
+      }
+
+      // Navigate back to the login screen
+      Navigator.pop(context);
+    } catch (error) {
+      log(error.toString());
+      // Handle signup errors here, e.g., email already exists
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Sign up failed: $error'),
+          duration: const Duration(seconds: 3),
+        ),
+      );
+    } finally {
+      setState(() {
+        isSigningUp = false; // Hide loading indicator
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
+    final double heightRatio = size.height / 915;
+    final double widthRatio = size.width / 412;
+
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: 100 * heightRatio,
+            ),
+            Text(
+              'Admin Sign Up',
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 40 * heightRatio * widthRatio,
+                fontFamily: 'Inter',
+                fontWeight: FontWeight.w700,
+                height: 0,
+              ),
+            ),
+            SizedBox(
+              height: 50 * heightRatio,
+            ),
+            Image.asset(
+              'images/logo.png',
+              height: 100 * heightRatio,
+              width: 100 * widthRatio,
+            ),
+            SizedBox(
+              height: 30 * heightRatio,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "E-Mail",
+                    style: TextStyle(
+                      fontSize: 18 * widthRatio * heightRatio,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10 * heightRatio,
+                  ),
+                  TextFormField(
+                    controller: emailController,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderSide: const BorderSide(
+                          color: Colors.grey,
+                        ),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(
+                          color: Colors.redAccent,
+                        ),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(
+                          color: Colors.redAccent,
+                        ),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 30 * heightRatio,
+                  ),
+                  Text(
+                    "Password",
+                    style: TextStyle(
+                      fontSize: 18 * widthRatio * heightRatio,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10 * heightRatio,
+                  ),
+                  TextFormField(
+                    controller: passwordController,
+                    obscureText: true, // Hide password text
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderSide: const BorderSide(
+                          color: Colors.grey,
+                        ),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(
+                          color: Colors.redAccent,
+                        ),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(
+                          color: Colors.redAccent,
+                        ),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 50 * heightRatio,
+                  ),
+                  SizedBox(
+                    height: 50 * heightRatio,
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      onPressed: isSigningUp
+                          ? null
+                          : _signUp, // Disable button during signup
+                      style: ElevatedButton.styleFrom(
+                        padding: EdgeInsets.symmetric(
+                            horizontal: 20.0 * widthRatio,
+                            vertical: 7.0 * heightRatio),
+                        backgroundColor: Colors.red[300],
+                        shape: const StadiumBorder(),
+                      ),
+                      child: isSigningUp
+                          ? const CircularProgressIndicator(
+                              valueColor:
+                                  AlwaysStoppedAnimation<Color>(Colors.white),
+                            )
+                          : Text(
+                              'Sign Up',
+                              style: TextStyle(
+                                fontSize: 20 * heightRatio * widthRatio,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 20 * heightRatio,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Already Have Account? ",
+                  style: TextStyle(
+                    color: const Color(0xFF6B6B6B),
+                    fontSize: 16 * widthRatio * heightRatio,
+                    fontFamily: 'Inter',
+                    fontWeight: FontWeight.w400,
+                    height: 0,
+                  ),
+                ),
+                InkWell(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const AdminLogin(),
+                      ),
+                    );
+                  },
+                  child: Text(
+                    'Login',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 16 * widthRatio * heightRatio,
+                      fontFamily: 'Inter',
+                      fontWeight: FontWeight.w700,
+                      decoration: TextDecoration.underline,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/Auth/employee_auth/employee_login.dart
@@ -0,0 +1,188 @@
+import 'package:battery_service_app/Auth/Admin_auth/adminlogin.dart';
+import 'package:flutter/material.dart';
+
+class EmployeeLogin extends StatefulWidget {
+  const EmployeeLogin({super.key});
+
+  @override
+  State<EmployeeLogin> createState() => _EmployeeLoginState();
+}
+
+class _EmployeeLoginState extends State<EmployeeLogin> {
+  @override
+  Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
+    final double heightRatio = size.height / 915;
+    final double widthRatio = size.width / 412;
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: 100 * heightRatio,
+            ),
+            Text(
+              'Employee Login',
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 40 * heightRatio * widthRatio,
+                fontFamily: 'Inter',
+                fontWeight: FontWeight.w700,
+                height: 0,
+              ),
+            ),
+            SizedBox(
+              height: 50 * heightRatio,
+            ),
+            Image.asset(
+              'images/logo.png',
+              height: 100 * heightRatio,
+              width: 100 * widthRatio,
+            ),
+            SizedBox(
+              height: 30 * heightRatio,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "E-Mail",
+                    style: TextStyle(
+                        fontSize: 18 * widthRatio * heightRatio,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(
+                    height: 10 * heightRatio,
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderSide: const BorderSide(
+                          color: Colors.grey,
+                        ),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(
+                          color: Colors
+                              .redAccent, // Change the border color to pink when focused
+                        ),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(
+                          color: Colors
+                              .redAccent, // Change the border color to pink when focused
+                        ),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 30 * heightRatio,
+                  ),
+                  Text(
+                    "Password",
+                    style: TextStyle(
+                        fontSize: 18 * widthRatio * heightRatio,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(
+                    height: 10 * heightRatio,
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderSide: const BorderSide(
+                          color: Colors.grey,
+                        ),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(
+                          color: Colors
+                              .redAccent, // Change the border color to pink when focused
+                        ),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(
+                          color: Colors
+                              .redAccent, // Change the border color to pink when focused
+                        ),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 50 * heightRatio,
+                  ),
+                  SizedBox(
+                    height: 50 * heightRatio,
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        padding: EdgeInsets.symmetric(
+                            horizontal: 20.0 * widthRatio,
+                            vertical: 7.0 * heightRatio),
+                        backgroundColor: Colors.red[300],
+                        shape: const StadiumBorder(),
+                      ),
+                      child: Text(
+                        'Login',
+                        style: TextStyle(
+                            fontSize: 20 * heightRatio * widthRatio,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20 * heightRatio,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Login As ",
+                        style: TextStyle(
+                          color: const Color(0xFF6B6B6B),
+                          fontSize: 16 * widthRatio * heightRatio,
+                          fontFamily: 'Inter',
+                          fontWeight: FontWeight.w400,
+                          height: 0,
+                        ),
+                      ),
+                      InkWell(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const AdminLogin()),
+                          );
+                        },
+                        child: Text(
+                          'Admin',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 16 * widthRatio * heightRatio,
+                            fontFamily: 'Inter',
+                            fontWeight: FontWeight.w700,
+                            decoration: TextDecoration.underline,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
